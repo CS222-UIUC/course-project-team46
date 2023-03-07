c31aacda54d276fe112b49a0ba4dcbd406c71b7b
BowenZ217 <bowzhu721@gmail.com>
2023-03-07 08:03:12 -0600

    Updated ReactDOM.render to createRoot as recommended in React 18.

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
-    document.getElementById("root")
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
