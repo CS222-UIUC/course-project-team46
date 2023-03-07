c208cdc15b498f4a682ab842047bb3a3d634b0d5
BowenZ217 <bowzhu721@gmail.com>
2023-03-07 09:11:14 -0600

    add app bar example from mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import SearchBar from './Components/SearchBar'
-import NavigationBar from './Components/NavigationBar';
+import AppBar from './Components/AppBar'
+// import NavigationBar from './Components/NavigationBar';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <NavigationBar />
+                <AppBar />
                 <SearchBar/>
             </header>
         </div>
