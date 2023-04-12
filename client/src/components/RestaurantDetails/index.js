4f109e7564a494856b5cb702dc222dc5181edbbf
XingZhao <xingz6@illinois.edu>
2023-04-12 15:30:04 -0500

    fix the format of the restaurant details

--- client/src/components/RestaurantDetails/index.js
+++ client/src/components/RestaurantDetails/index.js
@@ -1,6 +1,5 @@
-// RestaurantDetails.js
 import React from 'react';
-import { Typography, Grid, Link } from '@mui/material';
+import { Typography, Grid, Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from '@mui/material';
 
 const RestaurantDetails = ({ detail }) => {
     /**
@@ -15,48 +14,59 @@ const RestaurantDetails = ({ detail }) => {
      *      Saturday: 10 AM-8:30 PM
      */
     const renderWorkingHours = () => {
-        return Object.entries(detail.restaurant_working_hours).map(([day, hours]) => (
-            <Typography key={day} variant="body2">
-                {day}: {hours}
-            </Typography>
-        ));
+        return (
+        <Box>
+            <TableContainer component={Paper}>
+                <Table size="small" aria-label="working hours table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Day</TableCell>
+                        <TableCell>Opening Hours</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {Object.entries(detail.restaurant_working_hours).map(([day, hours]) => (
+                        <TableRow key={day}>
+                        <TableCell>{day}</TableCell>
+                        <TableCell>{hours}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>);
     };
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Website</Typography>
-                <Link href={detail.restaurant_site} target="_blank" rel="noopener noreferrer">
-                    {detail.restaurant_site}
-                </Link>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Phone</Typography>
-                <Typography>{detail.restaurant_phone}</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Address</Typography>
-                <Typography>{detail.restaurant_address}</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px', textAlign: 'left' }} >
+            <Typography variant="h6">Website</Typography>
+            <Link href={detail.restaurant_site} target="_blank" rel="noopener noreferrer">
+                {detail.restaurant_site}
+            </Link>
+
+            <Typography variant="h6">Phone</Typography>
+            <Typography>{detail.restaurant_phone}</Typography>
+
+            <Typography variant="h6">Address</Typography>
+            <Typography>{detail.restaurant_address}</Typography>
+
+            <Grid item xs={12} sm={6} sx={{ paddingRight: '20px' }}>
                 <Typography variant="h6">Working Hours</Typography>
                 {renderWorkingHours()}
             </Grid>
-            <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Description</Typography>
-                <Typography>{detail.restaurant_description}</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Booking Appointment</Typography>
-                <Link
-                    href={detail.restaurant_booking_appointment_link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Book a table
-                </Link>
-            </Grid>
-        </Grid>
+
+            <Typography variant="h6">Description</Typography>
+            <Typography>{detail.restaurant_description}</Typography>
+
+            <Typography variant="h6">Booking Appointment</Typography>
+            <Link
+                href={detail.restaurant_booking_appointment_link}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            Book a table
+            </Link>
+        </Box>
     );
 };
 
